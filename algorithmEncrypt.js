61c021cbd22b14e8fd798a6db97edfe6fc7cdeb1
cschallenberger <c.schallenberger@hotmail.com>
2017-04-21 13:36:49 -0300

    add funtion to encrypted values [BUG=function encrypt]

--- algorithmEncrypt.js
+++ algorithmEncrypt.js
@@ -2,16 +2,23 @@ function encrypt() {
     var textToEncrypt = document.getElementById("inputPrimary").value;
     var textEncrypted = "";
 
+// C (cifragem) => M^E mod N
+// arrumar aqui o for
     for(var i = 0; i < textToEncrypt.length; i++){
-        var charEncrypted = getValueByKey(textToEncrypt[i].toUpperCase());
+        var valueM = getValueByKey(textToEncrypt[i].toUpperCase());
+        var charEncrypted;
+
+        charEncrypted = (bigInt(valueM).pow(valueE)).divmod(valueN);
         textEncrypted += charEncrypted;
+        // alert(charEncrypted);
     }
+    // alert(valueE);
 
     setInputValue("inputPrimary", textEncrypted);
 }
 
 function getValueByKey(key) {
-    return hashAlphabet[key].toString();
+    return hashAlphabet[key];
 }
 
 function setInputValue(id, text) {
